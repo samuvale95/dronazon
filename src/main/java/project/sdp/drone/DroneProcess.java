a88c7519d7843d44e08b7951505ac7f6f46d3fdf
Samuele Valente <samuelevalente@gmail.com>
2021-05-09 23:33:11 +0200

    refactor add toString method

--- src/main/java/project/sdp/drone/DroneProcess.java
+++ src/main/java/project/sdp/drone/DroneProcess.java
@@ -4,10 +4,10 @@ import com.google.gson.Gson;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
-import javafx.util.Pair;
+import project.sdp.server.beans.Drone;
 import project.sdp.server.beans.ListDrone;
+import project.sdp.server.beans.Pair;
 
-import javax.swing.text.Position;
 import java.awt.*;
 import java.io.IOException;
 import java.util.Scanner;
@@ -27,7 +27,7 @@ import java.util.Scanner;
 * - DroneList
 *
 * */
-public class Drone {
+public class DroneProcess {
   private final int id;
   private final int port;
   private final String URI_AdmServer;
@@ -36,7 +36,7 @@ public class Drone {
   private ListDrone dronesList;
   private Boolean isMaster;
 
-  public Drone(int id, int port, String URI_AdmServer){
+  public DroneProcess(int id, int port, String URI_AdmServer){
       this.id = id;
       this.port = port;
       this.URI_AdmServer = URI_AdmServer;
@@ -46,23 +46,24 @@ public class Drone {
 
   private void registerToServer(){
       Client client = Client.create();
+      Drone newDrone = new Drone(this.id, "localhost", this.port);
+
       WebResource webResource = client.resource(URI_AdmServer + "/dronazon/drone/add");
-      ClientResponse clientResponse = webResource.post(ClientResponse.class);
+      ClientResponse clientResponse = webResource.type("application/json").post(ClientResponse.class, newDrone);
 
       if(clientResponse.getStatus() != 200)
           throw new RuntimeException("Something in your request is wrong " + clientResponse.getStatus());
 
       Gson gson = new Gson();
-      Pair<ListDrone, Point> pair = gson.fromJson(clientResponse.getEntity(String.class), Pair.class);
-      this.position = pair.getValue();
-      this.dronesList = pair.getKey();
+      String json = clientResponse.getEntity(String.class);
+      Pair pair = gson.fromJson(json, Pair.class);
+
+      this.dronesList = pair.getListDrone();
+      this.position = pair.getPosition();
   }
 
   public void start(){
       registerToServer();
-      System.out.println("Drone Info:");
-      System.out.println(dronesList);
-      System.out.println(position);
 
       try { System.in.read(); } catch (IOException e) { e.printStackTrace(); }
   }
@@ -75,7 +76,7 @@ public class Drone {
         System.out.println("Insert a communication port for a drone: ");
         int port = scanner.nextInt();
 
-        Drone drone = new Drone(id, port, "http://localhost:1337");
+        DroneProcess drone = new DroneProcess(id, port, "http://localhost:1337");
         drone.start();
     }
 }
