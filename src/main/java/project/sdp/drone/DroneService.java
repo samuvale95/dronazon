f6e7f9a7c74bef6f992534043611a88f81c1885b
Samuele Valente <samuelevalente@gmail.com>
2021-05-11 18:47:16 +0200

    create Drone service

--- src/main/java/project/sdp/drone/DroneService.java
+++ src/main/java/project/sdp/drone/DroneService.java
@@ -1,6 +1,30 @@
 package project.sdp.drone;
 
-import com.example.grpc.InsertServiceGrpc;
+import com.example.grpc.DroneServiceGrpc;
+import com.example.grpc.InsertMessageOuterClass.*;
+import io.grpc.stub.StreamObserver;
+import project.sdp.server.beans.Drone;
 
-public class InsertService extends InsertServiceGrpc.InsertServiceImplBase {
+public class DroneService extends DroneServiceGrpc.DroneServiceImplBase {
+
+    private final DroneProcess droneProcess;
+
+    public DroneService(DroneProcess droneProcess){
+        this.droneProcess = droneProcess;
+    }
+
+    @Override
+    public void insertIntoRing(InsertMessage request, StreamObserver<InsertMessage> responseObserver) {
+        InsertMessage.drone nextDroneMessage = request.getCallerDrone();
+        Drone nextDrone = new Drone(nextDroneMessage.getId(), nextDroneMessage.getIp(), nextDroneMessage.getPort());
+        droneProcess.setNextDrone(nextDrone);
+
+        System.out.println("My new next node is: ");
+        System.out.println(nextDrone);
+
+        Drone drone = droneProcess.getDrone();
+        InsertMessage.drone droneMessage = InsertMessage.drone.newBuilder().setId(drone.getId()).setIp(drone.getIp()).setPort(drone.getPort()).build();
+        responseObserver.onNext(InsertMessage.newBuilder().setCallerDrone(droneMessage).build());
+        responseObserver.onCompleted();
+    }
 }
