43907a90210af613c0acf1df62f1d46ea56c888c
Samuele Valente <samuelevalente@gmail.com>
2021-06-04 22:45:35 +0200

    refactor statistics

--- src/main/java/project/sdp/client/ClientAmministratore.java
+++ src/main/java/project/sdp/client/ClientAmministratore.java
@@ -1,11 +1,15 @@
 package project.sdp.client;
 
-import com.google.gson.Gson;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 import project.sdp.server.beans.ListDrone;
+import project.sdp.server.beans.Statistics;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ClientAmministratore {
@@ -20,34 +24,54 @@ public class ClientAmministratore {
                                 "3. Average number of Delivery\n" +
                                 "4. Average Distance traveled\n";
 
-    public void runClient(){
+    public ClientResponse RESTcall(String URL_path){
+        Client client = Client.create();
+        WebResource webResource = client.resource(BASE_URI + URL_path);
+        ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);
+        if(response.getStatus() != 200)
+            throw new RuntimeException("Something wrong on your request");
+        return response;
+    }
+
+    public void runClient() throws IOException {
         Scanner scanner = new Scanner(System.in);
         String option;
+        ObjectMapper objMap = new ObjectMapper();
 
         System.out.println(mainMenu);
         while (!(option = scanner.nextLine()).equals("quit")){
-            Client client = Client.create();
-            WebResource webResource = null;
-            ClientResponse response;
+
             String from, to, n;
+            TypeReference<ArrayList<Statistics>> typeRef;
 
             switch (option){
                 case "1":
-                    webResource = client.resource(BASE_URI + "/dronazon/drone/getAll");
+                    String dronesList = RESTcall("/dronazon/drone/getAll").getEntity(String.class);
+                    ListDrone list = objMap.readValue(dronesList, ListDrone.class);
+                    System.out.println("All drones inside smart city:");
+                    list.getDrones().forEach(System.out::println);
                     break;
                 case "2":
                     System.out.println("Insert number of statistics: ");
                     n = scanner.nextLine();
 
-                     webResource = client.resource(BASE_URI + "/dronazon/city/statistics/"+n);
-                     break;
+                    System.out.println("Print Statistics: ");
+                    String statistics = RESTcall("/dronazon/city/statistics/"+n).getEntity(String.class);
+                    typeRef = new org.codehaus.jackson.type.TypeReference<ArrayList<Statistics>>() {};
+                    ArrayList<Statistics> statsList = objMap.readValue(statistics, typeRef);
+                    statsList.forEach(System.out::println);
+                    break;
                 case "3":
                     System.out.println("Insert date from (dd-mm-yyyy): ");
                     from = scanner.nextLine();
                     System.out.println("Insert date to (dd-mm-yyyy): ");
                     to = scanner.nextLine();
 
-                    webResource = client.resource(BASE_URI + "/dronazon/city/statistics/delivery/from/"+from+"/to/"+to);
+                    System.out.println("Print delivery Statistics: ");
+                    String statsDelivery = RESTcall("/dronazon/city/statistics/delivery/from/"+from+"/to/"+to).getEntity(String.class);
+                    typeRef = new org.codehaus.jackson.type.TypeReference<ArrayList<Statistics>>() {};
+                    ArrayList<Statistics> DeliveryStatsList = objMap.readValue(statsDelivery, typeRef);
+                    DeliveryStatsList.forEach(System.out::println);
                     break;
                 case "4":
                     System.out.println("Insert date from (dd-mm-yyyy): ");
@@ -55,23 +79,20 @@ public class ClientAmministratore {
                     System.out.println("Insert date to (dd-mm-yyyy): ");
                     to = scanner.nextLine();
 
-                    webResource = client.resource(BASE_URI + "/dronazon/city/statistics/distance/from/"+from+"/to/"+to);
+                    System.out.println("Print distance Statistics: ");
+                    String statsDistance = RESTcall("/dronazon/city/statistics/distance/from/"+from+"/to/"+to).getEntity(String.class);
+                    typeRef = new org.codehaus.jackson.type.TypeReference<ArrayList<Statistics>>() {};
+                    ArrayList<Statistics> DistanceStatsList = objMap.readValue(statsDistance, typeRef);
+                    DistanceStatsList.forEach(System.out::println);
                     break;
             }
 
-            assert webResource != null;
-            response = webResource.accept("application/json").get(ClientResponse.class);
-            if(response.getStatus() != 200)
-                throw new RuntimeException("Something wrong on your request");
-
-            String drones = response.getEntity(String.class);
-            System.out.println(drones);
             System.out.println("\n");
             System.out.println(mainMenu);
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ClientAmministratore clientAmministratore = new ClientAmministratore();
         clientAmministratore.runClient();
     }
